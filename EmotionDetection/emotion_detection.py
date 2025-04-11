4c100d8da6098bb77531bff7e385405aef3cf11c
Aditya <adityasjain21@gmail.com>
2025-04-11 10:35:29 -0400

    task 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,7 +5,21 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers=headers, json=input_json)
-    format_text = json.loads(response.text)
-    emotions = format_text['emotionPredictions'][0]['emotion']
-    emotions['dominant_emotion'] = max(emotions.items(), key=lambda x: x[1])
+    
+    emotions = {}
+
+    if response.status_code == 200: 
+        format_text = json.loads(response.text)
+        emotions = format_text['emotionPredictions'][0]['emotion']
+        emotions['dominant_emotion'] = max(emotions.items(), key=lambda x: x[1])
+
+    elif response.status_code == 400:
+        emotions['anger'] = None
+        emotions['disgust'] = None
+        emotions['fear'] = None
+        emotions['joy'] = None
+        emotions['sadness'] = None
+        emotions['dominant_emotion'] = (None, None)
+    
     return emotions
+        
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This application provides interface for analysing the content for emotional sentimants sent by user.
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +8,36 @@ app = Flask('Emotion Detector')
 
 @app.route('/')
 def home():
+    """
+    Renders the index page to the user.
+    User can get analyse emotion of input string.
+
+    Returns:
+        html homepage
+    """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emotion_detector_endpoint():
-    textToAnalyze = request.args.get('textToAnalyze')
-    result = emotion_detector(textToAnalyze)
-    return_text = f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion'][0]}."
-    return result_text
+    """
+    Main logic to analyse a given string
+
+    This functions takes input string from GET request and process it for emotion under ML model.
+
+    Returns:
+        string: with emotional scores and dominant emotion with score as tuple.
+                returns None if input is invalid.
+    """
+    text_to_analyse = request.args.get('textToAnalyze')
+    result = emotion_detector(text_to_analyse)
+
+    if result['dominant_emotion'] is (None, None):
+        return "Invalid text! Please try again!"
+
+    return_text = f"""For the given statement, the system response is
+    'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}.
+    The dominant emotion is {result['dominant_emotion'][0]}."""
+    return return_text
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
-    app.run(debug=True)
